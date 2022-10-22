4884ef3c0a2ccfc7d60129861cd26b584585e9ee
hhh3167253066 <3167253066@qq.com>
2022-10-22 22:40:56 +0800

    add readme and minimist

--- allnpm/minimist_index.js
+++ allnpm/minimist_index.js
@@ -1,2 +1,11 @@
-var argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+#!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(0));
+console.log('argv---', argv);
+
+// 输入  node cmd.js xxx sss xxx sss xxx
+
+var argvGlo = require('minimist')(process.argv.slice(2), opts={});
+console.dir('argvGlo---', argvGlo);
+
+// node minimist_index.js -u root -p password password1 password2
+// node minimist_index.js -a -b testing
